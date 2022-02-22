645f2386f47dfc3433c93992c45dfb732ed452fe
hposedev <hposedev@gmail.com>
2022-02-22 12:44:15 +0100

    added recyclerView

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.Datasource
 import com.example.affirmations.databinding.ActivityMainBinding
 
@@ -11,7 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = ItemAdapter(this, myDataset)
 
-
+        recyclerView.setHasFixedSize(true)
+        //si tiene un numero de elementos fijos podemos mejorar su rendimiento
     }
 }
